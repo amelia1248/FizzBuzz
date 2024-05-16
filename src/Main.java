198aab37744b654c55834c46c4603888e49f6431
amelia1248 <ameliawu.wu@mail.utoronto.ca>
2024-05-16 18:53:09 -0400

    added proper argument in Main

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        FizzBuzz.doFizzBuzz(5);
+        String[] empty = new String[0];     // Creating an empty String array... note String Object type!!!
+        FizzBuzz.main(empty);
+    }
+
+}
