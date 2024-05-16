bad59545c295dfd8b7beea733693ce462c90dad2
amelia1248 <ameliawu.wu@mail.utoronto.ca>
2024-05-16 18:36:52 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,30 +5,41 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
+        for (int i = 1; i < 100; i++) {     // For all integers from 1 - 100 inclusive, iterating up by 1
 
             // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+            doFizzBuzz(i);
+        }
+    }
 
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
+    public static void doFizzBuzz(int i) {
+        boolean divisibleBy3 = i % 3 == 0;      // Creates boolean object if remainder is 0
+        boolean divisibleBy5 = i % 5 == 0;      // Creates boolean object if remainder is 0
 
-                System.out.println("Fizz Buzz");
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {     // && is logical AND operator
 
-            } else if (divisibleBy3) {
+            System.out.println("Fizz Buzz");
 
-                System.out.println("Fizz");
+        } else if (divisibleBy3) {      // Boolean object divisibleBy3 is True
 
-            } else if (divisibleBy5) {
+            System.out.println("Fizz");
 
-                System.out.println("Buzz");
+        } else if (divisibleBy5) {      // Boolean object divisibleBy5 is True
 
-            } else {
+            System.out.println("Buzz");
 
-                System.out.println(i);
+        } else {        // Else just print out integer i
 
-            }
+            System.out.println(i);
+
         }
     }
 }
+
+// NOTE: Differences with Python/Things specific to Java:
+// 1. && represents a Python and/logical and
+// 2. For for loops, instead of specifying the value we iterate by/using default "1" value, we use i++
+// 3. We need to declare a variable's object type before assigning it a value(eg. boolean divisibleBy3)
+// 4. If statements, for loops separate their body from rest of code using {}
+// 5. Built-in print function is written as System.out.println
---
+++ src/FizzBuzzWhile.java
@@ -0,0 +1,10 @@
+public class FizzBuzzWhile {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            FizzBuzz.doFizzBuzz(i);
+            i = i + 1;
+        }
+    }
+}
